58cc597820ad01ed1c682eba712e816b57652ab6
Erik Edlund <erik.edlund@32767.se>
2020-05-24 15:27:13 +0200

    Improve the Swagger documentation a little bit

--- Mashup.Api/Controllers/Api/V1/ArtistSummaryController.cs
+++ Mashup.Api/Controllers/Api/V1/ArtistSummaryController.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Mashup.Domain.Models.Rest.Consumed.MusicBrainz.Core;
+﻿using Mashup.Domain.Models.Rest.Consumed.MusicBrainz.Core;
 using Mashup.Domain.Models.Rest.Produced;
 using Mashup.Domain.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace Mashup.Api.Controllers.Api.V1
 {
     /// <summary>
     /// Artist Summaries.
     /// </summary>
+    [Produces("application/json")]
     [Route("api/v1/[controller]")]
     [ApiController]
     public class ArtistSummaryController : ApiControllerBase
@@ -33,7 +30,15 @@ namespace Mashup.Api.Controllers.Api.V1
         /// </summary>
         /// <param name="mbId">The MusicBrainzID.</param>
         /// <returns>The requested `ArtistSummary`.</returns>
+        /// <response code="200">Returns the `ArtistSummary`.</response>
+        /// <response code="400">If the given `mbId` is invalid.</response>
+        /// <response code="404">If the MusicBrainz Artist does not exist.</response>
+        /// <response code="503">If the MusicBrainz REST API imposed a rate limit.</response>
         [HttpGet("getone")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status503ServiceUnavailable)]
         public async Task<ArtistSummary> GetOne(
             [FromQuery] string mbId)
         {
